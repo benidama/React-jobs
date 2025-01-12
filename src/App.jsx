66b9ef646a9f3781277ce6f63e5fb48d3997cb3b
benidama <benidama02@gmail.com>
2025-01-12 23:46:03 +0000

    Errors solved

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,19 @@ import {
   createRoutesFromElements,
   RouterProvider,
 } from "react-router-dom";
-
+import MainLayout from "./layouts/MainLayout";
 import HomePage from "./pages/HomePage";
+import JobsPage from "./pages/JobsPage";
+import NotFoundPage from "./pages/NotFoundPage";
 
 const router = createBrowserRouter(
-  createRoutesFromElements(<Route index element={<HomePage />} />)
+  createRoutesFromElements(
+    <Route path="/" element={<MainLayout />}>
+      <Route index element={<HomePage />} />
+      <Route path="/jobs" element={<JobsPage />} />
+      <Route path="*" element={<NotFoundPage />} />
+    </Route>
+  )
 );
 const App = () => {
   return <RouterProvider router={router} />;
